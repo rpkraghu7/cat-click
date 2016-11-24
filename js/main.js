347a5cfd5e60355d78c31bf948c0a0984c66e68c
Pradeep Kumar R <pradeepraghu9427@gmail.com>
2016-11-24 09:24:57 +0530

    changes of karol

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 var elem1 = $("#image1");
 var elem2 = $("#image2");
-var score1 = $("#score1");
+var score = $("#score");
 var score2 = $("#score2");
 var catName = ['kitty1','kitty2'];
 
@@ -41,12 +41,11 @@ var cat = {
 
 
 
-  console.log(cat.cats.length);
+
   var buttons = function(){
     for (var i=0;i<cat.cats.length;i++)
     {
       var catname = cat.cats[i].name;
-      console.log(catname);
       var catt ='<button id="'+cat.cats[i].id+'" class="btn" type="button">'+catname+'<br>';
       $("#list").append(catt);
 
@@ -54,23 +53,51 @@ var cat = {
   };
 
   buttons();
-
-$(".btn").click(function(){
-  var id =$(this).attr('id');
-  $("#cat-image").append();
-});
+  var picture = $("#picture");
 
 
+  $(".btn").click(function(){
+    var id =$(this).attr('id');
+    $()
+    console.log(id);
+    $("#picture").remove();
+    switch (id) {
+      case cat.cats[0].id:
+      $("#cat-image").append('<img id="picture" class="image" src="'+cat.cats[0].url+'" alt="cats">');
+      console.log(cat.cats[0].count);
+      cat.cats[0].count++;
+      break;
+      case cat.cats[1].id:
+      $("#cat-image").append('<img id="picture" src="'+cat.cats[1].url+'" alt="cats">');
+      break;
+      case cat.cats[2].id:
+      $("#cat-image").append('<img id="picture" src="'+cat.cats[2].url+'" alt="cats">');
+      break;
+      case cat.cats[3].id:
+      $("#cat-image").append('<img id="picture" src="'+cat.cats[3].url+'" alt="cats">');
+      break;
+      case cat.cats[4].id:
+      $("#cat-image").append('<img id="picture" src="'+cat.cats[4].url+'" alt="cats">');
+      break;
+      default:
+      console.log('fail');
+      break;
+    }
+  });
 
+//console.log(cat.cats[1].id);
 
+ // $(".btn").click(function(){
+ //   var id =$(this).attr('id');
+ //   //$("#cat-image").append();
+ // });
 
+//var count=0;
 
-
-
-  // elem1.click(function(){
-  //   count1++;
-  //   score1.text(count1);
-  // });
+  $(".image").click(function(){
+  var count= cat.cats[0].count++;
+     score.text(count);
+  });
   // elem2.click(function(){
   //   count2++;
   //   score2.text(count2);
